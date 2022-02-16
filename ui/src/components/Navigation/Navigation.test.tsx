941256f776c4b350bde67826a813b81badb363ef
rich <ravergeek@gmail.com>
2022-02-16 23:09:49 +0000

    more unit tests

--- ui/src/components/Navigation/Navigation.test.tsx
+++ ui/src/components/Navigation/Navigation.test.tsx
@@ -1,3 +1,4 @@
+import { createStore } from '@reduxjs/toolkit';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
@@ -28,6 +29,31 @@ describe('<Navigation />', () => {
     expect(screen.getByTestId('register')).toBeInTheDocument();
     expect(screen.getByTestId('login')).toBeInTheDocument();
 
+
+  });
+
+
+
+
+
+
+  test("header logged in (redux dummy)", () => {
+
+
+
+    const reducer = (state: any, action: any) => {
+      switch (action.type) {
+
+
+        default:
+          return state;
+      }
+    }
+
+    const store = createStore(reducer, { auth: { name: "bob", loggedIn: true } });
+    render(<BrowserRouter><Provider store={store}><Navigation /></Provider></BrowserRouter>);
+    expect(screen.getByTestId('secure')).toBeInTheDocument();
+    expect(screen.getByTestId('logout')).toBeInTheDocument();
 
   });
 });
